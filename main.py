55c7c60666511626131584ec03d354e19ab91cc2
Danny Doan <dannykdoan@gmail.com>
2022-11-08 18:09:25 -0800

    main.py changes

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+
+documents = [] #temp for now will store all .jsons
+
+
+
+def buildIndex():
+    docID = 0
+    invertedIndex = dict()
+    for document in documents:
+        docID+=1
+        tokens = tokenize(document)
+        tokensWithNoDuplicate = set(tokens)
+        #build posting for document
+        docPosting = None #temp create posting (docid, frequency of token in that document)
+        for token in tokensWithNoDuplicate:
+            if token in invertedIndex.keys():
+                invertedIndex[token].append(docPosting)
+            else:
+                invertedIndex[token] = []
+    return invertedIndex
+        
+
+#todo: associate docIDs with urls
+
+
+        
+
+def tokenize():
+    #return a list of tokens (no duplicates)
+    return 0
