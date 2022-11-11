1ac2d37cd182f1b5a46fab8c727571eb3f73cbbf
Danny Doan <dannykdoan@gmail.com>
2022-11-10 23:13:00 -0800

    function definitions

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 
 
 
-documents = [] #temp for now will store all .jsons
-
-test = "lol"
 
 
+
+def getBatch(): 
+    #gets a batch of documents from /DEV, if there's no more documents it returns an empty list
+
+    return None
+
+def sortAndWriteToDisk(index, fileName):
+    #adds the index to a .txt file
+    return None
+    
 def buildIndex():
     docID = 0
     invertedIndex = dict()
-    for document in documents:
-        docID+=1
-        tokens = tokenize(document)
-        tokensWithNoDuplicate = set(tokens)
-        #build posting for document
-        docPosting = None #temp create posting (docid, frequency of token in that document)
-        for token in tokensWithNoDuplicate:
-            if token in invertedIndex.keys():
-                invertedIndex[token].append(docPosting)
-            else:
-                invertedIndex[token] = []
-    return invertedIndex
+    while True:
+        documentsInBatch = getBatch()
+        if not documentsInBatch:
+            break #end the loop if there's no more documents to process
+        for document in documentsInBatch:
+            docID+=1
+            tokens = tokenize(document)
+            tokensWithNoDuplicate = set(tokens)
+            #build posting for document
+            docPosting = None #temp create posting (docid, frequency of token in that document)
+            for token in tokensWithNoDuplicate:
+                if token in invertedIndex.keys():
+                    invertedIndex[token].append(docPosting)
+                else:
+                    invertedIndex[token] = []
+            
+        fileName = "disk.txt"
+        sortAndWriteToDisk(invertedIndex, fileName)
+        invertedIndex.clear()
+
+    return None
         
 
 #todo: associate docIDs with urls   
